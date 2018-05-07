71e7b00f4ea9a67866478c2f1c7006991d2cadb3
kwon <kajajin20@naver.com>
2018-05-07 14:43:26 +0900

    update

--- application/views/timeline/index.php
+++ application/views/timeline/index.php
@@ -119,21 +119,3 @@ function insert_submit(){
 <ul id='timeline'>
 
 </ul>
-<!--
-<ul id='timeline'>
-  <li class='work'>
-    <input class='radio' id='work1' name='works' type='radio' checked>
-    <div class="relative">
-      <label for='work5'>권성훈</label>
-      <span class='date'>12 May 2013</span>
-      <span class='circle'></span>
-    </div>
-    <div class='content'>
-	  <img src="/public/img/test2.jpg" width="800" height="200">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ea necessitatibus quo velit natus cupiditate qui alias possimus ab praesentium nostrum quidem obcaecati nesciunt! Molestiae officiis voluptate excepturi rem veritatis eum aliquam qui laborum non ipsam ullam tempore reprehenderit illum eligendi cumque mollitia temporibus! Natus dicta qui est optio rerum.
-      </p>
-    </div>
-  </li>
-</ul>
--->
